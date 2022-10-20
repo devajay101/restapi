0380152fae4056ead3f05aa968b003b5c44aa75a
Ajay <ajayxa1@gmail.com>
2022-10-20 18:30:12 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express=require("express")
+
+const app=express()
+
+app.get("/",(req,res)=>{
+
+    // call back function
+
+    
+
+}) 
+
+
+app.listen(2000,()=>{
+    console.log("server is running..");
+
+})
